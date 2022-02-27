7bcc2e0e7e9ebc5a648041d0bf25271a39cfe882
Peter Van Dyken <pvandyk2@uwo.ca>
2022-02-26 20:04:08 -0500

    Setup modules

---
+++ build.py
@@ -0,0 +1,15 @@
+from setuptools import Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension("intersection.src", sources=[
+        "src/cython/get_intersection.pyx",
+        "src/intersection.cpp",
+        "src/io.cpp"
+        ],  extra_compile_args=["-O0"])
+]
+
+def build(setup_kwargs):
+    setup_kwargs.update({
+        'ext_modules': cythonize(extensions, language_level=3)
+    })
