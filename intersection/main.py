f778b1fac1630a0877d14d964ba9b7c9e7528925
Peter Van Dyken <pvandyk2@uwo.ca>
2022-03-01 11:42:38 -0500

    Build CLI interface
    Include commands to run the intersection detection, and to output the
    results to a vtk file

--- intersection/main.py
+++ intersection/main.py
@@ -1,2 +1,37 @@
-import nibabel as nib
-arg
+from pathlib import Path
+
+import typer
+
+from intersection import src
+from intersection.models import Mesh, Results
+
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    left_surf: Path,
+    # right_hem: Path,
+    left_bundles: Path,
+    # right_bundles: Path,
+    out_path: Path,
+):
+    return src.get_intersection(left_surf, left_bundles, out_path)
+
+
+def get_intersection(surf_path: Path, bundles_path: Path, out_path: Path) -> int:
+    surf = Mesh.from_file(surf_path)
+    return src.intersections(
+        surf.points, surf.triangles, str(bundles_path), str(out_path)
+    )
+
+
+@app.command()
+def test(txt_file: Path, surface: Path, out_dir: Path):
+    results = Results.from_out_file(txt_file)
+    surf = Mesh.from_file(surface)
+    src_triangles = surf.filter_triangles(results.init_triangles)
+    dest_triangles = surf.filter_triangles(results.end_triangles)
+    out_dir.mkdir(exist_ok=True)
+    src_triangles.save_vtk(out_dir/"src.vtk")
+    dest_triangles.save_vtk(out_dir/"dest.vtk")
---
+++ intersection/models.py
@@ -0,0 +1,76 @@
+import itertools as it
+from pathlib import Path
+from typing import Type, TypeVar
+
+import attrs
+import nibabel as nib
+import numpy as np
+from fury.io import save_polydata
+from fury.utils import PolyData, set_polydata_triangles, set_polydata_vertices
+from numpy.typing import NDArray
+
+T = TypeVar("T")
+
+
+@attrs.frozen
+class Results:
+    num_fibers: int
+    init_triangles: list[int]
+    end_triangles: list[int]
+    init_intersection_points: list[float]
+    end_intersection_points: list[float]
+    fiber_indices: list[int]
+
+    @classmethod
+    def _get_list(cls, line: str, converter: Type[T] = int) -> list[T]:
+        return [converter(x) for x in line.split()]  # type: ignore
+
+    @classmethod
+    def from_out_file(cls, file: Path):
+        with file.open("r") as f:
+            data = f.read().split("\n")
+        return cls(
+            int(data[0]),
+            cls._get_list(data[1]),
+            cls._get_list(data[2]),
+            cls._get_list(data[3], float),
+            cls._get_list(data[4], float),
+            cls._get_list(data[5]),
+        )
+
+
+
+
+@attrs.frozen
+class Mesh:
+    points: NDArray[np.float32]
+    triangles: NDArray[np.int32]
+
+    def filter_triangles(self, whitelist: list[int]):
+        triangles = np.asarray(
+            [tri for i, tri in enumerate(self.triangles) if i in set(whitelist)],
+            self.triangles.dtype
+        )
+        needed_points = sorted(set(it.chain.from_iterable(triangles)))
+        indexed_points = dict(zip(needed_points, range(len(needed_points))))
+        for triangle in triangles:
+            for i, x in enumerate(triangle):
+                triangle[i] = indexed_points[x]
+        points = np.asarray([self.points[i] for i in needed_points], self.points.dtype)
+        return self.__class__(
+            points,
+            triangles
+        )
+
+    def save_vtk(self, name: Path):
+        pd = PolyData()
+        set_polydata_triangles(pd, self.triangles)
+        set_polydata_vertices(pd, self.points)
+        save_polydata(pd, str(name), binary=True)
+
+    @classmethod
+    def from_file(cls, name: Path):
+        data = nib.load(name)
+        return cls(
+            points=data.agg_data("pointset"), triangles=data.agg_data("triangle")
+        )
