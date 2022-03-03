76f5fc5fef7a325c66e5d3f1959beafd491013b0
Peter Van Dyken <pvandyk2@uwo.ca>
2022-03-03 12:52:58 -0500

    Calculate triangles formed by each vertex

---
+++ src/mesh.cpp
@@ -0,0 +1,25 @@
+#include "mesh.h"
+#include <iostream>
+
+vector<int>& Mesh::getTrianglesOfPoint(int point) {
+    if (this->trianglesOfPointsIndex.size() == 0) {
+        // Loop through every vertex; vertices are labelled by their position in the
+        // matrix
+        for (int i = 0; i < this->vertices.rows(); i++) {
+            this->trianglesOfPointsIndex.push_back(vector<int>());
+
+            // Loop through every triangle. Triangles are also labelled by their
+            // position in the matrix.
+            int triangleIndex = 0;
+            for (Vector3i polygon : this->polygons.rowwise()) {
+                auto index = find(polygon.begin(), polygon.end(), i);
+                if (index != polygon.end()) {
+                    this->trianglesOfPointsIndex.back().push_back(triangleIndex);
+                }
+                triangleIndex++;
+            }
+        }
+    }
+
+    return this->trianglesOfPointsIndex[point];
+}
