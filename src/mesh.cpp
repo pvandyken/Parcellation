d323082f138a185fff3d6bc36ce2f8c188f2f1de
Peter Van Dyken <pvandyk2@uwo.ca>
2022-03-04 16:31:35 -0500

    Optimize vertex-to-triangle index code

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -2,20 +2,13 @@
 
 const vector<int>& Mesh::getTrianglesOfPoint(int point) {
     if (this->trianglesOfPointsIndex.size() == 0) {
-        // Loop through every vertex; vertices are labelled by their position in the
-        // matrix
-        for (int i = 0; i < this->vertices.rows(); i++) {
-            this->trianglesOfPointsIndex.push_back(vector<int>());
-
-            // Loop through every triangle. Triangles are also labelled by their
-            // position in the matrix.
-            int triangleIndex = 0;
-            for (Vector3i polygon : this->polygons.rowwise()) {
-                auto index = find(polygon.begin(), polygon.end(), i);
-                if (index != polygon.end()) {
-                    this->trianglesOfPointsIndex.back().push_back(triangleIndex);
-                }
-                triangleIndex++;
+        // Initialize the index according to the number of points
+        trianglesOfPointsIndex.resize(this->vertices.rows());
+        // Loop through every triangle
+        for (size_t i = 0; i<this->polygons.rows(); i++)
+        {
+            for (auto point : polygons(i, all)) {
+                trianglesOfPointsIndex[point].push_back(i);
             }
         }
     }
